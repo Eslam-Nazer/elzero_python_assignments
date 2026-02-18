80537294ee2a392fc060f040cfea6f665c8c0545
Eslam-Nazer <eslam.nazer43@gmail.com>
2026-02-18 02:00:07 +0200

    Answers of assignments from 001 to 010

---
+++ From001to010/index.py
@@ -0,0 +1,18 @@
+# ------------------------------------- #
+#       Assignment: From 001 to 010     #
+# ------------------------------------- #
+
+name = "eslam";
+age = "25";
+country = "Egypt";
+# ------------------------------------- #
+print("Name:" + name)
+print("Age:" + age)
+print("Country: " + country)
+# ------------------------------------- #
+print("Hello, My Name Is " + name + " And Iam {:s} Years Old and I Live in {:s}.".format(age, country))
+# ------------------------------------- #
+print(type(name))
+print(type(age))
+print(type(country))
+# ------------------------------------- #
