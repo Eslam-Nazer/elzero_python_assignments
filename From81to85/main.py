8227f6e763da7572c13b7d9cc02d85f59c3d941c
Eslam-Nazer <eslam.nazer43@gmail.com>
2026-02-27 03:14:19 +0200

    Assignment: From 081 to 085

---
+++ From81to85/main.py
@@ -0,0 +1,32 @@
+# ------------------------------------- #
+# ----- Assignment: From 081 to 085 --- #
+# ------------------------------------- #
+
+def reverse_string(my_string):
+    for char in reversed(my_string):
+        yield char
+
+
+for c in reverse_string("Elzero"):
+    print(c)
+
+# ------------------------------------------ #
+print('-' * 50)
+
+def myDecorator(func):
+    def wrapper():
+        print("Sugar Added From Decorators")
+
+        func()
+        print("#" * 20)
+    return wrapper
+
+@myDecorator
+def make_tea():
+  print("Tea Created")
+@myDecorator
+def make_coffe():
+  print("Coffe Created")
+
+make_tea()
+make_coffe()
